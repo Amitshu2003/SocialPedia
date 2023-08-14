ecdc054162ae69e09026b65757e5295825e46723
Amitshu2003 <201951021@iiitvadodara.ac.in>
2023-08-14 14:46:08 +0530

    add code

---
+++ src/components/Home/Home.js
@@ -0,0 +1,91 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from "@material-ui/core"
+import Posts from "../Posts/Posts"
+import Form from "../Form/Form"
+import Paginate from '../Pagination'
+import { useNavigate, useLocation } from 'react-router-dom'
+import useStyles from "./styles"
+import ChipInput from 'material-ui-chip-input'
+import myContext from '../../Context/MyContext'
+
+function useQuery() {
+    // console.log(useLocation().search);
+    return new URLSearchParams(useLocation().search)
+}
+
+function Home() {
+    const [currentId, setCurrentId] = useState(null)
+    const navigate = useNavigate()
+    const query = useQuery()
+    const page = query.get('page')
+    const searchQuery = query.get('searchQuery')
+    const classes = useStyles()
+    const [search, setSearch] = useState("")
+    const [tags, setTags] = useState([])
+    const { getPosts, getPostBySearch } = useContext(myContext)
+
+    useEffect(() => {
+        getPosts()
+    }, [])
+
+    const handleKeyDown = (e) => {
+        if (e.keyCode === 13) searchPost()
+    }
+
+    const handleAdd = (tag) => {
+        setTags([...tags, tag])
+    }
+    const handleDelete = (tagtoDelete) => {
+        setTags(tags.filter((tag) => tag !== tagtoDelete))
+    }
+
+    const searchPost = () => {
+        if (search.trim() || tags) {
+            getPostBySearch({ search, tags: tags.join(",") })
+            navigate(`/posts/search?searchQuery=${search || 'none'}&tags=${tags.join(",")}`)
+        } else {
+            navigate("/")
+        }
+    }
+
+    return (
+        <Grow in style={{ padding: 0 }}>
+            <Container maxWidth='xl'>
+                <Grid container justifyContent="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+
+                    <Grid item xs={12} sm={3} md={3}>
+                        <AppBar className={classes.appBarSearch} position="static" color="inherit">
+                            <TextField name="search" variant="outlined" label="Search By Title" fullWidth value={search} onChange={(e) => { setSearch(e.target.value) }}
+                                onKeyDown={handleKeyDown} />
+                            <ChipInput
+                                style={{ margin: '10px 0' }}
+                                value={tags}
+                                onAdd={handleAdd}
+                                onDelete={handleDelete}
+                                label="Search By Tags"
+                                variant="outlined"
+                            />
+                            <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
+                        </AppBar>
+
+                        <Paper elevation={6} className={classes.pagination}>
+                            <Paginate page={page} />
+                        </Paper>
+                    </Grid>
+
+                    <Grid item xs={12} sm={6} md={6}>
+                        <Posts setCurrentId={setCurrentId} />
+                    </Grid>
+
+                    <Grid item xs={12} sm={3} md={3}>
+                        <Form currentId={currentId} setCurrentId={setCurrentId} />
+
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+
+    )
+}
+
+export default Home
---
+++ src/components/Pagination.js
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect } from 'react'
+import { Pagination, PaginationItem } from "@material-ui/lab"
+import useStyles from './styles'
+import { Link } from 'react-router-dom'
+import myContext from '../Context/MyContext'
+
+function Paginate({page}) {
+    const classes = useStyles()
+    const {numberOfPages, getPosts} = useContext(myContext)
+
+    useEffect(()=>{
+        if(page){
+            getPosts(page)
+        }
+    },[page])
+    return (
+        <Pagination
+            classes={{ ul: classes.ul }}
+            count={numberOfPages}
+            page={Number(page) || 1}
+            variant="outlined"
+            color="primary"
+            renderItem={(item) => (
+                <PaginationItem {...item} component={Link} to={`/posts?page=${item.page}`} />
+            )}
+        />
+    )
+}
+
+export default Paginate
